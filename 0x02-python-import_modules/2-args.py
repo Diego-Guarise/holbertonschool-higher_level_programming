5f8cb3784eb0e1f2e785b101486042ee4d71e8f0
Diego-Guarise <l.guariselopez@gmail.com>
2021-09-09 15:06:04 +0000

    v2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+number = len(argv)
+if argv == 0:
+    print("{} argument.".format(number))
+else:
+    print("{}: argument:".format(number))
+i = 0
+while i < number:
+    print("{}: argv[i]".format(number))
