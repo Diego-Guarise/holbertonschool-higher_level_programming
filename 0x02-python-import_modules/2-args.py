0f6d0420a1fec318bd4cdd735b9917d76ed99c87
Diego-Guarise <l.guariselopez@gmail.com>
2021-09-09 16:18:56 +0000

    v2.3

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,19 +1,16 @@
 #!/usr/bin/python3
-from sys import argv, exit
+from sys import argv
 if __name__ == "__main__":
     number = len(argv) - 1
     if number == 0:
         print("{} argument.".format(number))
-        exit
     elif number == 1:
         print("{}: argument:".format(number))
         print("{}: {}".format(number, argv[1]))
-        exit
     else:
         print("{} arguments:".format(number))
         i = 1
         while i <= number:
             print("{}: {}".format(i, argv[i]))
             i += 1
-if __name__ == "__main__":
-    from sys import argv, exit
+
