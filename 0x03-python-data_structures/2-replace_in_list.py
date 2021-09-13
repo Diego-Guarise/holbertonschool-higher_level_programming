cd974aefd4eea48d22e1faca3092adeba9955c15
Diego-Guarise <l.guariselopez@gmail.com>
2021-09-13 16:21:27 +0000

    v1

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,6 +1,6 @@
 def replace_in_list(my_list, idx, element):
     if idx < 1 or idx > len(my_list):
-        return (my_list)
+        return my_list
     else:
         my_list[idx] = element
-        return (my_list)
+        return my_list
