c55893f611e51445620f8e639741d5afdceef30e
Diego-Guarise <l.guariselopez@gmail.com>
2021-09-13 16:41:25 +0000

    #!

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def element_at(my_list, idx):
     i = len(my_list)
     if idx < 0:
--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     if idx < 1 or idx > len(my_list):
         return my_list
