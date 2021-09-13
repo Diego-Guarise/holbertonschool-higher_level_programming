4292bca19c52f4916439d950abe343aa93e6ab52
Diego-Guarise <l.guariselopez@gmail.com>
2021-09-13 18:48:29 +0000

    v1.1

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+def no_c(my_string):
+    if my_string:
+        lase = "Cc"
+        for lase in lase:
+            my_string = my_string.replace(lase, '')
+    return my_string
