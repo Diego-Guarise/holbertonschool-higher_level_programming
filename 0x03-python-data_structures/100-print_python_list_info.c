5b190ab09b22e17d08db668fc7158e68ecadc668
Diego-Guarise <l.guariselopez@gmail.com>
2021-09-14 13:45:50 -0700

    v1

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,6 @@
+/**
+ * print_python_list_info - funtion
+ * @p: pyobjet
+ */
+void print_python_list_info(PyObject *p)
+{}
--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,7 +3,32 @@
 #include "lists.h"
 int is_palindrome(listint_t **head)
 {
+    int count = 1, i;
+    listint_t *run1, *run2, *new, *new2;
+
     if (!head)
         return(NULL);
-    while(head && )
+    run = *head;
+    while(run->next)
+    {
+        run = run->next;
+        count++;
+    }
+    run2 = *head;
+    for(i = 0; i != (count/2); i++)
+    {
+        run2 = run2->next;
+        if(i + 1 == (count/2))
+        {
+             new = run2;
+             new->next = run2->next;
+        }
+    }
+    new2 = *head
+    for(x = 0; x != (count/2); x++)
+    {
+        if(new->n != new2->n)
+            return(0);
+    }
+    return(1);
 }
