a8ae47691cf369f1a6869c8ddc14caf2c1ad07e6
Diego-Guarise <l.guariselopez@gmail.com>
2021-09-14 12:21:08 -0700

    v1

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,37 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
-/**
- * is_palindrome - palindrome
- * @head: head
- * Return: int
- */
 int is_palindrome(listint_t **head)
 {
-    listint_t *h = *head;
-
-    int b[1024], i, j;
-    
-    i = 0;
-    j = 0;
-
-    if(*head != NULL)
-    {
-        while (h)
-        {
-            b[i] = h->n;
-            h = h->next;
-            i++;
-        }
-        while (j < i / 2)
-        {
-            if(b[j] == b[i - j - 1])
-            {
-                j++;
-            }
-            else
-            {
-                return(0);
-            }
-        }
-    }
-    return (1);
+    if (!head)
+        return(NULL);
+    while(head && )
 }
