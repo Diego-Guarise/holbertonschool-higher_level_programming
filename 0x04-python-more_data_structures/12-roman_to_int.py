8ae53d358f0f458bbe8fe73fe9bc340bba7ce6ab
Diego-Guarise <l.guariselopez@gmail.com>
2021-09-15 16:09:12 -0700

    v1

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     romannum = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    if not roman_string:
-        return 0
+    num = []
+    suma = 0
+    for letras in roman_string:
+        for i, x in romannum.items():
+            if letras == i:
+                num.append(x)
+    for j in num:
+        if j < (j+1):
+            suma += -j
+        elif j > (j+1):
+            suma += j
+        else:
+            suma += j
+    return -suma
