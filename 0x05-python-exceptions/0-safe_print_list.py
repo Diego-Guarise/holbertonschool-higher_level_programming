380eaa349d0a8382dfc57a53af189950e3b4248c
Diego-Guarise <l.guariselopez@gmail.com>
2021-09-27 09:45:24 -0700

    v1

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,10 @@
+def safe_print_list(my_list=[], x=0):
+    j = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            j+=1
+        except:
+            pass
+    print()
+    return j
