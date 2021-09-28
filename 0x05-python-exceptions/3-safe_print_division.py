3e492ff675874e7da90da454fda11edfe40c4669
Diego-Guarise <l.guariselopez@gmail.com>
2021-09-27 18:01:37 -0700

    v1

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except (ZeroDivisionError, FloatingPointError):
+        result = None
+    finally:
+        print("{}".format(result))
+        return result
