e7d0fcd8efe4a1188fb6b7f72087da5cd439af41
Diego-Guarise <l.guariselopez@gmail.com>
2021-09-28 15:47:19 -0700

    v1

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,32 +1,25 @@
 #!/usr/bin/python3
-    """Represents a square.
-    Private instance attribute: size:
-        - property def size(self)
-        - property setter def size(self, value)
-    Instantiation with optional size.
-    Public instance method: def area(self).
-    Public instance method: def my_print(self).
-    """
+"""Represents a square."""
 
 
 class Square:
     """Initializes the data."""
-    def __init__(self, size = 0):
+    def __init__(self, size=0):
         self.__size = size
 
     def area(self):
         """Returns the current square area."""
-	return self.__size * self.__size
+        return self.__size * self.__size
 
     @property
     def size(self):
         """Retrieves the size."""
-	return self.__size
+        return self.__size
 
     @size.setter
     def size(self, value):
         """Sets the size to a value."""
-	if type(value) is not int:
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
@@ -35,7 +28,7 @@ class Square:
 
     def my_print(self):
         """Prints to stdout the square with the character #."""
-	if self.__size == 0:
+        if self.__size == 0:
             print()
         else:
             for i in range(self.__size):
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,36 +1,36 @@
 #!/usr/bin/python3
-    """Represents a square.
-    Private instance attribute: size:
-        - property def size(self)
-        - property setter def size(self, value)
-    Private instance attribute: position:
-        - property def position(self)
-        - property setter def position(self, value)
-    Instantiation with optional size and optional position.
-    Public instance method: def area(self).
-    Public instance method: def my_print(self).
-    """
+"""Represents a square.
+Private instance attribute: size:
+    - property def size(self)
+    - property setter def size(self, value)
+Private instance attribute: position:
+    - property def position(self)
+    - property setter def position(self, value)
+Instantiation with optional size and optional position.
+Public instance method: def area(self).
+Public instance method: def my_print(self).
+"""
 
 
 class Square:
     """Initializes the data."""
-    def __init__(self, size = 0, position=(0, 0)):
-	self.__size = size
-	self.__position = position
+    def __init__(self, size=0, position=(0, 0)):
+        self.__size = size
+        self.__position = position
 
     def area(self):
         """Returns the current square area."""
-	return self.__size * self.__size
+        return self.__size * self.__size
 
     @property
     def position(self):
         """Retrieves the position."""
-	return self.__position
+        return self.__position
 
     @position.setter
     def position(self, value):
         """Sets the position to a value."""
-	if len(value) != 2:
+        if len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         elif type(value) != tuple:
             raise TypeError("position must be a tuple of 2 positive integers")
@@ -40,14 +40,13 @@ class Square:
     @property
     def size(self):
         """Sets the size to a value."""
-	return self.__size
+        return self.__size
 
     @size.setter
     def size(self, value):
         """Prints to stdout the square with the character #,
-        at the position given by the position attribute.
-        """
-	if type(value) is not int:
+        at the position given by the position attribute."""
+        if type(value) is not int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
