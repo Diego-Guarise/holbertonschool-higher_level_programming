320d1e411b78e311c260f35740772239fbe027d8
Diego-Guarise <l.guariselopez@gmail.com>
2021-10-12 14:18:32 -0700

    v1

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" Write a function
+"""
+
+
+class MyList(list):
+    """Class Mylist"""
+
+    def print_sorted(self):
+        """print sorted"""
+        print(sorted(self))
