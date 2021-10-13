dbef01170c304ac3cdd65bbba43048757e197d40
Diego-Guarise <l.guariselopez@gmail.com>
2021-10-12 18:04:09 -0700

    v1

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
+"""create a class"""
+
+
 class MyInt(int):
+    """define"""
     def __init__(self, value):
         self.num = value
 
     def __eq__(self, other):
+        """eq"""
         return self.num != other
 
     def __ne__(self, other):
+        """ne"""
         return self.num == other
--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""define"""
+
+
 def add_attribute(*args):
+    """add"""
     if "main" in str(type(args[0])):
         setattr(args[0], args[1], args[2])
     else:
