e9c2f771489b35234e65a5db0c94e8f29d4d0ab5
Diego-Guarise <l.guariselopez@gmail.com>
2021-10-12 17:34:33 -0700

    v1

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -13,7 +13,8 @@ class Square(Rectangle):
         """size must be private. No getter or setter
         size must be a positive integer, validated by integer_validator
         """
-        self.__size = self.integer_validator("size", size)
+        self.integer_validator("size", size)
+        self.__size = size
         super().__init__(self.__size, self.__size)
 
     def __str___(self):
