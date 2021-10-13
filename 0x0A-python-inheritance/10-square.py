b436af4874bfc74557510b6d2e204f85337a43bd
Diego-Guarise <l.guariselopez@gmail.com>
2021-10-12 17:54:50 -0700

    v1

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -14,4 +14,5 @@ class Square(Rectangle):
         size must be a positive integer, validated by integer_validator
         """
         self.integer_validator("size", size)
-        super().__init__(size, size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
