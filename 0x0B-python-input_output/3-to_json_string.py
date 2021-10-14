c9810d0e5d0c79b6dacd5366a6c47b03468ac74f
Diego-Guarise <l.guariselopez@gmail.com>
2021-10-13 17:17:13 -0700

    v1

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Write a function that returns the JSON
+representation of an object (string):"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """jeison"""
+    return json.dumps(my_obj)
