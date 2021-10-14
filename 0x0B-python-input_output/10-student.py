d139aeb66631d8e28f5146f6fe3a2561493f41c4
Diego-Guarise <l.guariselopez@gmail.com>
2021-10-13 17:59:49 -0700

    v1

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Write a class Student that defines a student by:"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """Public instance attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """return"""
+        lis = {}
+        if type(attrs) is list:
+            for i in attrs:
+                try:
+                    lis[i] = getattr(self, i)
+                except Exception:
+                    pass
+            return lis
+        else:
+            return self.__dict__
