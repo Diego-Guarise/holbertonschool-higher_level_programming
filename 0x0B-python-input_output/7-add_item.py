08d2c62886fbc6d3f7e8f2088495c088b409b4f2
Diego-Guarise <l.guariselopez@gmail.com>
2021-10-14 17:32:54 -0700

    v1

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,6 +7,8 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 from sys import argv
 
-
-lista = []
-
+try:
+    lista = load_from_json_file("add_item.json") + argv[1:]
+    save_to_json_file(lista, "add_item.json")
+except Exception:
+     save_to_json_file(argv[1:], "add_item.json")
