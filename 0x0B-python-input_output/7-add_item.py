dc4fa44d660ad3324261f0ac9ac8b7af7f4ffe0c
Diego-Guarise <l.guariselopez@gmail.com>
2021-10-14 17:36:29 -0700

    v1

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,12 +3,13 @@
 and then save them to a file:"""
 
 
+from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-from sys import argv
+
 
 try:
     lista = load_from_json_file("add_item.json") + argv[1:]
     save_to_json_file(lista, "add_item.json")
 except Exception:
-     save_to_json_file(argv[1:], "add_item.json")
+    save_to_json_file(argv[1:], "add_item.json")
