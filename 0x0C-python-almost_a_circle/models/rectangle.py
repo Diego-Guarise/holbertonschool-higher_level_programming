73d9ad23eaff45083009c0bf08f841988a8e4086
Diego-Guarise <l.guariselopez@gmail.com>
2021-10-21 10:37:20 -0700

    pep

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -98,7 +98,7 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = y
 
-#    def area(self):
+    def area(self):
 #        """colo"""
 
 #        return self.width * self.height
