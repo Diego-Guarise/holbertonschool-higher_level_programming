e7bb805c11a40c61b409bba073a8ab12f8680ab9
Diego-Guarise <l.guariselopez@gmail.com>
2021-10-21 10:16:43 -0700

    correccion rectangle terminada

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -98,64 +98,69 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = y
 
-#   def area(self):
+    def area(self):
+        """colo"""
 
-#        return self.__width * self.__height
+        return self.__width * self.__height
 
-#    def display(self):
+    def display(self):
+        """colo"""
 
-#        for a in range(0, self.__height):
-#            for b in range(0, self.__width):
-#                print("#", end="")
-#            print("")
+        for a in range(0, self.__height):
+            for b in range(0, self.__width):
+                print("#", end="")
+            print("")
 
-#    def __str__(self):
+    def __str__(self):
+        """colo"""
 
-#        strr = "[Rectangle] ({}) {}/{} - {}/{}".format(
-#            self.id, self.__x, self.__y, self.__width, self.__height)
-#        return strr
+        strr = "[Rectangle] ({}) {}/{} - {}/{}".format(
+            self.id, self.__x, self.__y, self.__width, self.__height)
+        return strr
 
-#    def display(self):
+    def display(self):
+        """colo"""
 
-#        for f in range(0, self.__y):
-#            print()
-#        for c in range(0, self.__height):
-#            for d in range(0, self.__x):
-#                print(" ", end="")
-#            for e in range(0, self.__width):
-#                print("#", end="")
-#            print()
+        for f in range(0, self.__y):
+            print()
+        for c in range(0, self.__height):
+            for d in range(0, self.__x):
+                print(" ", end="")
+            for e in range(0, self.__width):
+                print("#", end="")
+            print()
 
-#    def update(self, *args, **kwargs):
+    def update(self, *args, **kwargs):
+        """colo"""
 
-#        if args is not None and len(args) != 0:
-#            if len(args) >= 0:
-#                if type(args[0]) != int:
-#                    raise TypeError("id must be an integer")
-#                self.id = args[0]
-#            if len(args) > 1:
-#                self.width = args[1]
-#            if len(args) > 2:
-#                self.height = args[2]
-#            if len(args) > 3:
-#                self.x = args[3]
-#            if len(args) > 4:
-#                self.y = args[4]
-#        else:
-#            for key, value in kwargs.items():
-#                if key == "id":
-#                    self.id = value
-#                if key == "width":
-#                    self.width = value
-#                if key == "height":
-#                    self.height = value
-#                if key == "x":
-#                    self.x = value
-#                if key == "y":
-#                    self.y = value
+        if args is not None and len(args) != 0:
+            if len(args) >= 0:
+                if type(args[0]) != int:
+                    raise TypeError("id must be an integer")
+                self.id = args[0]
+            if len(args) > 1:
+                self.width = args[1]
+            if len(args) > 2:
+                self.height = args[2]
+            if len(args) > 3:
+                self.x = args[3]
+            if len(args) > 4:
+                self.y = args[4]
+        else:
+            for key, value in kwargs.items():
+                if key == "id":
+                    self.id = value
+                if key == "width":
+                    self.width = value
+                if key == "height":
+                    self.height = value
+                if key == "x":
+                    self.x = value
+                if key == "y":
+                    self.y = value
 
-#    def to_dictionary(self):
-
-#        my_dict = {'id': self.id, 'width': self.__width,
-#                   'height': self.__height, 'x': self.__x, 'y': self.__y}
-#        return my_dict
+    def to_dictionary(self):
+        """colo"""
+        my_dict = {'id': self.id, 'width': self.__width,
+                   'height': self.__height, 'x': self.__x, 'y': self.__y}
+        return my_dict
