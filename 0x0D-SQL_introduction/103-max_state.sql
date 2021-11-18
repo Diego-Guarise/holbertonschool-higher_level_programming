105f6692dc1218d32a99151b0a66b2f85a075718
Diego-Guarise <l.guariselopez@gmail.com>
2021-11-18 15:42:42 -0800

    task 20

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,2 +1,2 @@
 -- Write a script that displays the max temperature of eachate (ordered by State name).
-SELECT state, Max(value) AS max_temp FROM temperatures GROUP BYate ORDER BYate ASC LIMIT 3;
+SELECT state, Max(value) AS max_temp FROM temperatures GROUP BY state ORDER BY state ASC LIMIT 3;
