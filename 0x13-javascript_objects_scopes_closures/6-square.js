25af6ae56206fbb8bd807719fd8333bd8e583d2a
Diego-Guarise <l.guariselopez@gmail.com>
2021-12-14 16:55:43 +0000

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,8 +1,18 @@
 #!/usr/bin/node
-const Square = require('./5-square');
-module.exports = class Square extends Re {
-  constructor (size) {
-    super(size, size);
-    this.size = size;
+const sq = require('./5-square');
+module.exports = class Square extends sq {
+  charPrint (c) {
+    let a = '';
+    for (let step = 0; step < this.height; step++) {
+      for (let paso = 0; paso < this.width; paso++) {
+        if (!c) {
+          a += 'X' + '';
+        } else {
+          a += 'C' + '';
+        }
+      }
+      console.log(a);
+      a = '';
+    }
   }
 };
