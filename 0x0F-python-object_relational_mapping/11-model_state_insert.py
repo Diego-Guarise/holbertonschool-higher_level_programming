e1dc0dacaafddfa64be0c204e1818225d4ec56c4
Diego-Guarise <l.guariselopez@gmail.com>
2021-12-19 20:22:10 +0000

    task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Get state"""
+
+
+if __name__ == "__main__":
+    import sys
+    from model_state import Base, State
+    from sqlalchemy.orm import Session
+    from sqlalchemy import (create_engine)
+
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'
+        .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+        pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    session.add(State(name='Louisiana'))
+    session.commit()
+    state = session.query(State).order_by(State.id)\
+        .filter(State.name == 'Louisiana').all()
+    print("{}".format(state.id))
+    session.close()
