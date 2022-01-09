d7b321fd54deba907be37e24e4c2a60c4fecdaa3
Diego-Guarise <l.guariselopez@gmail.com>
2022-01-09 18:05:00 -0300

    fix

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #Write a Bash script that sends a DELETE request to the URL passed
-#as the first argument and displays the body of the response
-
-curl -sLX '$1' 'DELETE'
+curl -sX "DELETE" '$1'
--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #Write a Bash script that takes in a URL and
-#displays all HTTP methods the server will accept.
-
-curl -siLX OPTIONS '$1'
+curl -siLX OPTIONS '$1' | grep "Allow" | cut -d " " -f2-
