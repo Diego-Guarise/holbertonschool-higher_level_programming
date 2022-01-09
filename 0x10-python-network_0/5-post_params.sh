e04e3f1cfe28337c186fe50ee35d0fd9054980e0
Diego-Guarise <l.guariselopez@gmail.com>
2022-01-09 18:13:06 -0300

    -d

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Write a Bash script that takes in a URL, sends a POST
-curl -sX POST -d "email=test@gmail.com" "subject=I will always be here for PLD" "$1"
+curl -sX POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD" "$1"
