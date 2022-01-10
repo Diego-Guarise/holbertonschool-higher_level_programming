1384b50a778f128d00c772f2d4d276aabcb4033c
Diego-Guarise <l.guariselopez@gmail.com>
2022-01-09 22:32:33 -0300

    try to .format

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -14,4 +14,4 @@ if __name__ == "__main__":
         with request.urlopen(argv[1]) as response:
             print(response.read().decode('utf-8'))
     except error.HTTPError as e:
-        print('Error code: ', e.code)
+        print("Error code: {:}".format(e.code))
