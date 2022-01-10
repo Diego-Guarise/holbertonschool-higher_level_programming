b4107ea05cea9d0c247d1b5593e568c55148252c
Diego-Guarise <l.guariselopez@gmail.com>
2022-01-09 21:21:14 -0300

    fix import

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -3,9 +3,11 @@
 Write a Python script that fetches
 https://intranet.hbtn.io/status
 """
-import urllib.request
+
 
 if __name__ == "__main__":
+    import urllib.request
+
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         html = response.read()
         print('Body response:')
