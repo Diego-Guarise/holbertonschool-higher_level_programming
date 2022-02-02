a167db3ae1b1f92a8250f1bf14dabfd7c033bd81
Diego-Guarise <l.guariselopez@gmail.com>
2022-02-02 13:12:48 -0300

    task 0 | Fix semistandar

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
-var fs = require('fs');
-fs.readFile(process.argv[2], 'utf8', function(err, data){
-    if (data){
-        console.log(data);
-    }else{
-        console.log(err);
-    }
-});
+const fs = require('fs');
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+  if (data) {
+    console.log(data);
+  } else {
+    console.log(err);
+  }
+})
+;
