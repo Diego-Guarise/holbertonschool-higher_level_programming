4398c5067ae84bde33332ddfc4a8c018f4e5e288
Diego-Guarise <l.guariselopez@gmail.com>
2022-02-02 22:40:34 -0300

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,6 +7,15 @@ request(url, function (error, res, body) {
     return;
   }
   const films = JSON.parse(body).results;
-  const
-  console.log(text.title);
+  const id = 18;
+  let count = 0;
+  for (const tmp in films) {
+    const links = films[tmp].characters;
+    for (const compare in links) {
+      if (links[compare].includes(id)) {
+        count++;
+      }
+    }
+  }
+  console.log(count);
 });
